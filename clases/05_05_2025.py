198997690fae49af2eef3df4e4b015f459f432d2
SARD <sardlord82b.12@gmail.com>
2025-05-05 20:48:09 -0600

    Actualización trabajo clase 05_05_2025

--- clases/05_05_2025.py
+++ clases/05_05_2025.py
@@ -17,4 +17,23 @@ lista = []
 for i in range(5):
     valor = input("dame un valor ")
     lista.append(valor)
+
 print(lista)
+
+lista1 = ['p', "paraguas", 69, 3]
+print(lista1)
+lista1.remove('p')
+print(lista1)
+lista1.pop()
+print(lista1)
+
+lista2 = ['a', 'b', 'c']
+lista3 = ['e', 'f', 'g']
+print(lista2 + lista3)
+
+import matplotlib.pyplot as plt #as es para el alias
+x = [1,2,3,4,5]
+y = [2,4,1,3,5]
+
+plt.plot(x, y)
+plt.title("Gráfica")
